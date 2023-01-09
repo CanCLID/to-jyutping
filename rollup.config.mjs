884a0ecff595fa1cf591560f3cdfb12f4afdb265
graphemecluster <graphemecluster@gmail.com>
2023-01-09 18:25:36 +0800

    Reduce Module Size, Update Dependencies

---
+++ rollup.config.mjs
@@ -0,0 +1,28 @@
+import typescript from "rollup-plugin-typescript2";
+import { string } from "rollup-plugin-string";
+import terser from "@rollup/plugin-terser";
+import dts from "rollup-plugin-dts";
+import del from "rollup-plugin-delete";
+
+/** @type { import("rollup").RollupOptions[] } */
+export default [
+  {
+    input: "./src/index.ts",
+    output: {
+      file: "./dist/index.js",
+      format: "umd",
+      exports: "named",
+      name: "ToJyutping",
+      sourcemap: true,
+    },
+    plugins: [typescript(), string({ include: "**/*.txt" }), terser()],
+  },
+  {
+    input: "./dist/index.d.ts",
+    output: {
+      file: "./dist/index.d.ts",
+      format: "es",
+    },
+    plugins: [dts(), del({ targets: ["./dist/*", "!./dist/index.*"], hook: "buildEnd" })],
+  },
+];
