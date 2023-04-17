fa36e2a57f665148ef02c973761629af9326a66a
graphemecluster <graphemecluster@gmail.com>
2023-04-18 06:16:17 +0800

    Add methods for retrieving all possible pronunciations of a character

--- preprocess.ts
+++ preprocess.ts
@@ -1,41 +1,96 @@
-import { spawnSync } from "child_process";
-import { readFileSync, writeFileSync } from "fs";
+import { createReadStream, createWriteStream } from "fs";
+import { createInterface } from "readline";
+import { readdir } from "fs/promises";
+import { resolve } from "path";
 import { OpenCC } from "opencc";
-import Trie from "./src/Trie";
+import { pipeline } from "stream/promises";
+import Trie from "./Trie";
 
-spawnSync("curl -o dict/char.csv https://raw.githubusercontent.com/CanCLID/rime-cantonese-upstream/main/char.csv");
-spawnSync("curl -o dict/word.csv https://raw.githubusercontent.com/CanCLID/rime-cantonese-upstream/main/word.csv");
+(async () => {
+  const rankDict: Record<string, number> = {
+    預設: 0,
+    常用: 1,
+    罕見: 2,
+    棄用: 3,
+  };
 
-const data = new Map<string, [string, number]>();
-for (const path of ["./dict/char.csv", "./dict/word.csv"]) {
-  for (const line of readFileSync(path, { encoding: "utf-8" }).split("\n").slice(1, -1)) {
-    const [字, 粵拼, freq] = line.split(",");
-    if (/[ -~]/.test(字)) continue;
-    const 詞頻 = freq ? (freq.slice(-1) === "%" ? +freq.slice(0, -1) * 0.01 : +freq) : 0.07;
-    if (isNaN(詞頻)) continue;
-    const length = Array.from(字).length;
-    if (length !== 粵拼.split(" ").length && length !== 1) continue;
-    const 元字 = data.get(字);
-    if (元字) {
-      const [元粵拼, 元詞頻] = 元字;
-      if (!(詞頻 > 元詞頻 || (詞頻 === 元詞頻 && 元粵拼.slice(-1) !== "2" && 粵拼.slice(-1) === "2"))) continue;
-    }
-    data.set(字, [粵拼, 詞頻]);
+  const data = new Map<string, Map<string, number>>();
+
+  const rl = createInterface({
+    input: createReadStream("./rime-cantonese-upstream/char.csv"),
+    crlfDelay: Infinity,
+  })[Symbol.asyncIterator]();
+  await rl.next();
+
+  for await (const line of rl) {
+    const [char, jyutping, pronRank] = line.split(",");
+    if (/[ -~]/.test(char)) continue;
+    const freq = rankDict[pronRank];
+    const map = data.get(char);
+    if (map) {
+      const originalFreq = map.get(jyutping) ?? -1;
+      if (freq > originalFreq) map.set(jyutping, freq);
+    } else data.set(char, new Map([[jyutping, freq]]));
   }
-}
 
-const map = new Map<string, string>();
-const converter = new OpenCC("t2s.json");
-data.forEach(([粵拼], 字) => {
-  map.set(字, 粵拼);
-  map.set(converter.convertSync(字), 粵拼);
-});
+  for (const path of await readdir("./rime-cantonese-upstream")) {
+    if (path === "char.csv" || !path.endsWith(".csv")) continue;
+    const rl = createInterface({
+      input: createReadStream(resolve("./rime-cantonese-upstream", path)),
+      crlfDelay: Infinity,
+    })[Symbol.asyncIterator]();
+    const columns = (await rl.next()).value.split(",");
+    const charColumn = columns.indexOf("char");
+    const jyutpingColumn = columns.indexOf("jyutping");
+    if (charColumn === -1 || jyutpingColumn === -1) continue;
 
-let result = "";
-const trie = new Trie();
-map.forEach((粵拼, 字) => {
-  result += 字 + "\t" + 粵拼 + "\n";
-  trie.set(字, 粵拼);
-});
-writeFileSync("./dict/dictionary.txt", result);
-writeFileSync("./dict/trie.txt", trie.serialize());
+    for await (const line of rl) {
+      const values = line.split(",");
+      const char = values[charColumn];
+      if (/[ -~]/.test(char)) continue;
+      const jyutping = values[jyutpingColumn];
+      if (Array.from(char).length !== jyutping.split(" ").length) continue;
+      const map = data.get(char);
+      if (map) {
+        if (!map.has(jyutping)) map.set(jyutping, 4);
+      } else data.set(char, new Map([[jyutping, 4]]));
+    }
+  }
+
+  const dict = new Map(
+    Array.from(data, ([char, map]) => {
+      const freqToJyutping: string[][] = [];
+      for (const [jyutping, freq] of map) {
+        if (!(freq in freqToJyutping)) freqToJyutping[freq] = [jyutping];
+        else freqToJyutping[freq].push(jyutping);
+      }
+      return [
+        char,
+        freqToJyutping.flatMap(rank =>
+          rank
+            .map(jyutping => [
+              jyutping,
+              jyutping
+                .split(" ")
+                .map(u => " 213546"[+u[u.length - 1]] || "_")
+                .join(""),
+            ])
+            .sort(([, left], [, right]) => +(left > right) || -(left < right))
+            .map(([jyutping]) => jyutping)
+        ),
+      ];
+    })
+  );
+
+  const converter = new OpenCC("t2s.json");
+  await Promise.all(
+    Array.from(dict, ([char, map]) => converter.convertPromise(char).then(result => dict.set(result, map)))
+  );
+  await pipeline(
+    Array.from(dict, ([char, jyutpings]) => char + "\t" + jyutpings.join("\t") + "\n"),
+    createWriteStream("./dict/dictionary.txt")
+  );
+  const trie = new Trie();
+  for (const line of dict) trie.set(...line);
+  await new Promise(resolve => createWriteStream(`dict/trie.txt`).write(trie.serialize(), resolve));
+})();
