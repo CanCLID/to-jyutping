1024fa020008a9ce068fd369a17e19ec93be9d19
graphemecluster <graphemecluster@gmail.com>
2024-09-27 02:52:45 +0800

    Refactor: Move `decodeJyutping` to separate file

--- src/Trie.ts
+++ src/Trie.ts
@@ -1,4 +1,5 @@
 import data from "./trie.txt";
+import { decodeJyutping } from "./utils";
 
 type Node = Map<string, Node> & { v?: string[]; m?: WeakMap<Trie, string[] | null | undefined> };
 
@@ -99,33 +100,5 @@ export class CustomizableTrie extends Trie {
 		// The use of `has` and ternary operators are intentional to test if a user
 		// explicitly disallow the entry by setting the value to `null` or `undefined`
 		return n.m ? (n.m.has(this) ? n.m.get(this) : this.#parent.getValue(n)) : n.v;
-	}
-}
-
-const onset = ["", "b", "p", "m", "f", "d", "t", "n", "l", "g", "k", "ng", "gw", "kw", "w", "h", "z", "c", "s", "j"];
-const nucleus = ["aa", "a", "e", "i", "o", "u"];
-const rhyme = ["oe", "oen", "oeng", "oet", "oek", "eoi", "eon", "eot", "yu", "yun", "yut", "m", "ng"];
-const coda = ["", "i", "u", "m", "n", "ng", "p", "t", "k"];
-
-function decodeJyutping(s: string) {
-	return Array.from(iteratePairs(s), ([x, y]) => {
-		const order = (x.charCodeAt(0) - 33) * 90 + (y.charCodeAt(0) - 33);
-		const final = ~~((order % 402) / 6);
-		return (
-			onset[~~(order / 402)]
-			+ (final >= 54 ? rhyme[final - 54] : nucleus[~~(final / 9)] + coda[final % 9])
-			+ ((order % 6) + 1)
-		);
-	}).join(" ");
-}
-
-function* iteratePairs(s: string) {
-	const it = s[Symbol.iterator]();
-	for (;;) {
-		const x = it.next();
-		if (x.done) return;
-		const y = it.next();
-		if (y.done) return;
-		yield [x.value, y.value] as const;
 	}
 }
