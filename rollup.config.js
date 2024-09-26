e918906383b226da8edfbf162777d91dc36e3da4
graphemecluster <graphemecluster@gmail.com>
2024-09-27 05:28:05 +0800

    Don’t export type of `Trie`; Simplify declaration

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import typescript from "rollup-plugin-typescript2";
 import { string } from "rollup-plugin-string";
 import terser from "@rollup/plugin-terser";
+import modify from "rollup-plugin-modify";
 import dts from "rollup-plugin-dts";
 import del from "rollup-plugin-delete";
 
@@ -37,6 +38,11 @@ export default [
 			file: "./dist/index.d.ts",
 			format: "es",
 		},
-		plugins: [dts(), del({ targets: ["./dist/*", "!./dist/index.*"], hook: "buildEnd" })],
+		plugins: [
+			modify({ find: /\s*#private;\s*constructor\(t: Trie\);/, replace: "" }),
+			modify({ find: /get (\w+).*?$/m, replace: (_, $1) => `readonly ${$1}: typeof ${$1};` }),
+			dts(),
+			del({ targets: ["./dist/*", "!./dist/index.*"], hook: "buildEnd" }),
+		],
 	},
 ];
